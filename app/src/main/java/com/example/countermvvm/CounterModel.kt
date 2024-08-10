c216d6ed3020d52c87233151204c535dc102a404
Utsav Pathak <utsavpathak59@gmail.com>
2024-08-10 11:42:20 +0530

    App coompleted TO-DO : add readme

---
+++ app/src/main/java/com/example/countermvvm/CounterModel.kt
@@ -0,0 +1,16 @@
+package com.example.countermvvm
+
+data class CounterModel(var count : Int)
+
+class CounterRepository{
+    private var _counter = CounterModel(0)
+
+    fun getCounter() = _counter
+
+    fun incrementCounter(){
+        _counter.count++
+    }
+    fun decrementCounter(){
+        _counter.count--
+    }
+}
